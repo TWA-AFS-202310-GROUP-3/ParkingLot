20c0b61414bad27fdd1bbb71766364cb4c26c077
Ke <kwang97@slb.com>
2023-11-03 16:32:11 +0800

    feat&test: implement AC2 that including FetchCar and ParkCar methods in ParkingLotManager and the tests of them

---
+++ ParkingLot/ParkingLotManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    public class ParkingLotManager
+    {
+        private string car = "Benze";
+        private Dictionary<string, string> ticketToCar = new Dictionary<string, string>();
+
+        public string FetchCar(string ticket)
+        {
+            var car = ticketToCar[ticket];
+            return car;
+        }
+
+        public string ParkCar(string car)
+        {
+            var ticket = $"T_{car}";
+            ticketToCar[ticket] = car;
+            return ticket;
+        }
+    }
+}
