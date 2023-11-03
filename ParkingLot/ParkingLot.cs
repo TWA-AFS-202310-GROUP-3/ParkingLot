69b74319bfbd67e7d751aa1b7d73cf8579b03123
YChen110 <Ychen110@slb.com>
2023-11-03 16:14:15 +0800

    feat(story 1 AC 1 & 2): add parkingLot park and fetch logic, allow park multiple cars and fetch corresponding car

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,24 @@
+﻿namespace ParkingLotProj
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ParkingLot
+    {
+        private string car;
+        private Dictionary<string, string> parkingTicket = new Dictionary<string, string>();
+        public string Fetch(string ticket)
+        {
+            string car = parkingTicket[ticket];
+            parkingTicket.Remove(ticket);
+            return car;
+        }
+
+        public string Park(string car)
+        {
+            string ticketId = Guid.NewGuid().ToString();
+            parkingTicket.Add(ticketId, car);
+            return ticketId;
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,35 @@
+﻿using ParkingLotProj;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_Get_Same_Car_When_Fetch_By_Ticket()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+
+            string ticket = parkingLot.Park("car");
+            string car = parkingLot.Fetch(ticket);
+
+            Assert.Equal("car", car);
+        }
+
+        [Fact]
+        public void Should_Get_Correct_Car_When_Fetch_By_Corresponding_Ticket()
+        {
+            string car1 = "car1";
+            string car2 = "car2";
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket1 = parkingLot.Park(car1);
+            string ticket2 = parkingLot.Park(car2);
+
+            string actualCar1 = parkingLot.Fetch(ticket1);
+            string actualCar2 = parkingLot.Fetch(ticket2);
+
+            Assert.Equal(car1, actualCar1);
+            Assert.Equal(car2, actualCar2);
+        }
+    }
+}
