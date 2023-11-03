b1a4c05f59b5b86fe4b6e7093b809841645eedf7
Wang Jing <jwang315@slb.com>
2023-11-03 16:47:13 +0800

    feat: implement ParkingLot that can park and fetch

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotPractice
+{
+    public class ParkingLot
+    {
+        private string car;
+        private int maxcapacity = 10;
+        private Dictionary<string, string> ticket2Car = new Dictionary<string, string>();
+        public string Fetch(string ticket)
+        {
+            if (!ticket2Car.ContainsKey(ticket))
+            {
+                return null;
+            }
+            else
+            {
+                string fetchedCar = ticket2Car[ticket];
+                ticket2Car.Remove(ticket);
+                return fetchedCar;
+            }
+        }
+
+        public string Fetch()
+        {
+            return null;
+        }
+
+        public string Park(string car)
+        {
+            if (ticket2Car.Count == maxcapacity)
+            {
+                return null;
+            }
+
+            string ticket = "T-" + car;
+            ticket2Car.Add(ticket, car);
+            return ticket;
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ParkingLotPractice;
+
+namespace ParkingLotTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_get_the_same_car_when_fetch_car_by_ticket()
+        {
+            // Given
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket = parkingLot.Park("car");
+            string expectedResult = "car";
+            // When
+            string result = parkingLot.Fetch(ticket);
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_get_correct_car_when_fetch_by_corresponding_ticket()
+        {
+            // Given
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket1 = parkingLot.Park("car1");
+            string ticket2 = parkingLot.Park("car2");
+            string expectedResult = "car2";
+            // When
+            string result = parkingLot.Fetch(ticket2);
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_get_no_car_when_ticket_is_wrong()
+        {
+            // Given
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket1 = parkingLot.Park("car1");
+            string ticket2 = "T-car2";
+            string expectedResult = null;
+            // When
+            string result = parkingLot.Fetch(ticket2);
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_get_no_car_when_ticket_is_empty()
+        {
+            // Given
+            ParkingLot parkingLot = new ParkingLot();
+            string expectedResult = null;
+            // When
+            string result = parkingLot.Fetch();
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_get_no_car_when_ticket_is_used_before()
+        {
+            // Given
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket = parkingLot.Park("car");
+            string car = parkingLot.Fetch(ticket);
+            string expectedResult = null;
+            // When
+            string result = parkingLot.Fetch(ticket);
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_fail_to_park_when_capacity_is_full()
+        {
+            // Given
+            ParkingLot parkingLot = new ParkingLot();
+            for (int i = 0; i < 10; i++)
+            {
+                parkingLot.Park("car" + i.ToString());
+            }
+
+            string expectedResult = null;
+            // When
+            string result = parkingLot.Park("car");
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
