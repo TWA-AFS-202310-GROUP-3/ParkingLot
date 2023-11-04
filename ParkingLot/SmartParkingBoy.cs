509655d0fa6b0011f97e22c872908abc8822c3d9
Wang Jing <jwang315@slb.com>
2023-11-04 21:09:30 +0800

    feat: implement smart parking boy that can park in parkingLot with more positions.

---
+++ ParkingLot/SmartParkingBoy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotPractice
+{
+    public class SmartParkingBoy
+    {
+        private ParkingLot[] parkingLots;
+        public SmartParkingBoy(params ParkingLot[] parkingLots)
+        {
+            this.parkingLots = parkingLots;
+        }
+
+        public string Park(string car)
+        {
+            return parkingLots.MaxBy(p => p.GetAvailablePositionAmount()).Park(car);
+        }
+
+        public string Fetch(string ticket)
+        {
+            foreach (var parkingLot in parkingLots)
+            {
+                try
+                {
+                    return parkingLot.Fetch(ticket);
+                }
+                catch { }
+            }
+
+            throw new WrongTicketException("Unrecognized parking ticket. ");
+        }
+    }
+}
