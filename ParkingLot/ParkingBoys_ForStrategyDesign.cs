277117bc5cf3781ed250bbb463b0eb1f2942b47b
Ke <kwang97@slb.com>
2023-11-05 15:48:34 +0800

    feat&test: implement story6 feature and test case

---
+++ ParkingLot/ParkingBoys_ForStrategyDesign.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    public class ParkingBoys_ForStrategyDesign
+    {
+        private ParkingBoyBase parkingboy;
+        public ParkingBoyBase ParkingBoy
+        {
+            get => parkingboy;
+            set => parkingboy = value;
+        }
+
+        public ParkingBoys_ForStrategyDesign(ParkingBoyBase parkingBoy)
+        {
+            this.parkingboy = parkingBoy;
+        }
+
+        public string FetchCar(string ticket)
+        {
+            return this.parkingboy.FetchCar(ticket);
+        }
+
+        public string ParkCar(string car)
+        {
+            return this.parkingboy.ParkCar(car);
+        }
+    }
+}
